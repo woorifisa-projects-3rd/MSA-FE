993512a9367848c3293a000ff4349f480fcbda3e
apple6346654 <apple6346654@naver.com>
2024-11-12 11:13:24 +0900

    feat:직원 출퇴근 조회페이지 구현

--- app/(with-sidebar)/attendance/daily-attendance/page.js
+++ app/(with-sidebar)/attendance/daily-attendance/page.js
@@ -2,11 +2,68 @@
 
 import AttendanceModalBody from '@/components/modal/attendance-modal/attendance-modal-body';
 import ModalContainer from '@/components/modal/modal-container';
+import DefaultTable from '@/components/table/DefaultTable';
 import { useState } from 'react';
 
 
 export default function Form() {
-  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false); 
+
+  const tableHeaders = {
+    no: "No.",
+    name: "직원이름",
+    startTime: "출근시간",
+    endTime: "퇴근시간", 
+    totalHours: "총 근무시간",
+    salary: "급여금액",
+    edit: "수정",
+    delete: "삭제"
+};
+
+const list = [
+  {
+      no: "01",
+      name: "정성윤",
+      startTime: "14:00",
+      endTime: "18:04",
+      totalHours: "04:04",
+      salary: "38000원",
+     
+  },
+  {
+      no: "02",
+      name: "류혜리",
+      startTime: "08:58",
+      endTime: "15:03",
+      totalHours: "06:05",
+      salary: "60000원",
+  },
+  {
+      no: "02",
+      name: "류혜리",
+      startTime: "08:58",
+      endTime: "15:03",
+      totalHours: "06:05",
+      salary: "60000원",
+  },
+  {
+      no: "02",
+      name: "류혜리",
+      startTime: "08:58",
+      endTime: "15:03",
+      totalHours: "06:05",
+      salary: "60000원",
+  },
+  {
+      no: "02",
+      name: "류혜리",
+      startTime: "08:58",
+      endTime: "15:03",
+      totalHours: "06:05",
+      salary: "60000원",
+  }
+];
+
 
   
   return (
@@ -21,6 +78,7 @@ export default function Form() {
       >
         <AttendanceModalBody/>
       </ModalContainer>
+      <DefaultTable tableName="출퇴근 조회" tableHeaders={tableHeaders} list={list}/>
    
     </>
   );
