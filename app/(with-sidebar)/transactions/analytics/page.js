31b87f043abbd3335dade370f5971bcbf048f673
Hyunah Lee <hyuna030619@naver.com>
2024-11-12 15:27:53 +0900

    feat: 매출, 지출 카테고리별 원형그래프 구현

--- app/(with-sidebar)/transactions/analytics/page.js
+++ app/(with-sidebar)/transactions/analytics/page.js
@@ -1,25 +1,84 @@
 'use client';
 
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Doughnut } from 'react-chartjs-2';
 import classes from "./page.module.css";
-import ModalContainer from '@/components/modal/modal-container';
-import WorkplaceModal from '@/components/modal/workplace-registraion/workplace-registration';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Colors } from 'chart.js';
+import BaseButton from '@/components/button/base-button';
+
+ChartJS.register(ArcElement, Tooltip, Legend, Colors);
+
+const generateDummyData = () => {
+  return {
+    sales: [
+      { category: '일일 매출 입금', amount: 5000000 },
+      { category: '카드 매출 정산금', amount: 2000000 },
+      { category: '현금 매출 입금', amount: 1000000 },
+      { category: '온라인 결제 정산', amount: 800000 },
+    ],
+    expenses: [
+      { category: '임대료', amount: 2000000 },
+      { category: '인건비', amount: 1500000 },
+      { category: '관리비', amount: 2500000 },
+      { category: '도시가스비', amount: 1200000 },
+      { category: '카드수수료', amount: 500000 },
+    ],
+  };
+};
 
 export default function SalesExpenses() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const { sales, expenses } = generateDummyData();
 
-    return (
-        <div className={classes.container}>
-            <h1 className={classes.title}>금융 상품 추천 페이지</h1>
-            <button onClick={() => setIsModalOpen(true)}>모달 열기</button>
-            <ModalContainer
-                isOpen={isModalOpen}
-                onClose={()=>setIsModalOpen(false)}
-                title="사업장 등록"
-            >
-               {/* 여기메 모달  content 넣기  */}
-               <WorkplaceModal />
-            </ModalContainer>
+  const salesData = {
+    labels: sales.map(item => item.category),
+    datasets: [
+      {
+        label: '매출',
+        data: sales.map(item => item.amount),
+        hoverOffset: 6,
+      },
+    ],
+  };
+
+  const expensesData = {
+    labels: expenses.map(item => item.category),
+    datasets: [
+      {
+        label: '지출',
+        data: expenses.map(item => item.amount),
+        hoverOffset: 6,
+      },
+    ],
+  };
+
+  const options = {
+    responsive: true, // 반응형 동작
+    plugins: {
+      legend: {
+        position: 'bottom' // 범례를 차트 하단에 배치
+      }
+    },
+  };
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.chartContainer}>
+        <div>
+          <h2>매출</h2>
+          <Doughnut data={salesData} options={options} />
         </div>
-    );
+        <div>
+          <h2>지출</h2>
+          <Doughnut data={expensesData} options={options} />
+        </div>
+      </div>
+      <div className={classes.salesContainer}>
+        일, 주, 월 매출 차트
+      </div>
+      <div className={classes.reportsContainer}>
+        <BaseButton text="손익계산서 발급"/>
+        <BaseButton text="간편장부 발급"/>
+      </div>
+    </div>
+  );
 }
