fb110de7735b0d57fb44694ecca2eee8c1805516
apple6346654 <apple6346654@naver.com>
2024-11-18 12:21:39 +0900

    feat:급여기록조회계좌이미지추가

--- app/(with-sidebar)/payroll-auto-transfer/records/page.js
+++ app/(with-sidebar)/payroll-auto-transfer/records/page.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import Button from "@/components/button/button";
 import classes from "./page.module.css";
@@ -6,13 +6,13 @@ import DefaultTable from '@/components/table/DefaultTable';
 import MonthSelector from "@/components/selector/selector";
 import { useState } from 'react';
 import NameSearch from "@/components/namesearch/name-search";
-import Sorting from "@/components/sorting/Sorting";
+import Sorting from "@/components/sorting/sorting";
 import BaseButton from "@/components/button/base-button";
+import { bankCodeList } from "@/constants/bankCodeList";
 
 const buttonText = "확인";
 
 export default function PayRecords() {
-
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [sortOption, setSortOption] = useState('latest'); // 최신순 디폴트
     const [searchQuery, setSearchQuery] = useState('');
@@ -30,12 +30,12 @@ export default function PayRecords() {
     };
 
     const [list, setList] = useState([
-        { name: '정성윤', account: '110-123456-45687', amount: '266,000원', date: '2024/10/03', button: <BaseButton text={buttonText}  /> },
-        { name: '이현아', account: '213-151-1223165', amount: '309,210원', date: '2024/11/03', button: <BaseButton text={buttonText}  /> },
-        { name: '류혜리', account: '111-15795-246821', amount: '309,210원', date: '2024/09/03', button:<BaseButton text={buttonText}  />},
-        { name: '임지혁', account: '258-1467-284567', amount: '309,210원', date: '2023/10/03', button: <BaseButton text={buttonText}  /> },
-        { name: '박준현', account: '258-1467-284567', amount: '309,210원', date: '2024/10/05', button:  <BaseButton text={buttonText}  />},
-        { name: '강세필', account: '258-1467-284567', amount: '309,210원', date: '2024/10/09', button: <BaseButton text={buttonText}  /> },
+        { name: '정성윤', account: '110-123456-45687', code: '020', amount: '266,000원', date: '2024/10/03', button: <BaseButton text={buttonText} /> },
+        { name: '이현아', account: '213-151-1223165', code: '003', amount: '309,210원', date: '2024/11/03', button: <BaseButton text={buttonText} /> },
+        { name: '류혜리', account: '111-15795-246821', code: '081', amount: '309,210원', date: '2024/09/03', button: <BaseButton text={buttonText} /> },
+        { name: '임지혁', account: '258-1467-284567', code: '088', amount: '309,210원', date: '2023/10/03', button: <BaseButton text={buttonText} /> },
+        { name: '박준현', account: '258-1467-284567', code: '045', amount: '309,210원', date: '2024/10/05', button: <BaseButton text={buttonText} /> },
+        { name: '강세필', account: '258-1467-284567', code: '090', amount: '309,210원', date: '2024/10/09', button: <BaseButton text={buttonText} /> },
     ]);
 
     // 선택된 년, 월과 검색어에 맞는 데이터 필터링
@@ -60,6 +60,28 @@ export default function PayRecords() {
         }
     });
 
+    // bankCodeList와 매칭하여 SVG 추가한 리스트 생성
+    const updatedList = sortedList.map(item => {
+        const bank = bankCodeList.find(bank => bank.code === item.code); // 코드로 은행 찾기
+        const updatedAccount = bank ? (
+            <span style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+                <div
+                    className={classes.bankIcon} // SVG에 스타일 추가
+                    dangerouslySetInnerHTML={{ __html: bank.logoUrl }} // SVG 삽입
+                />
+                <span>{item.account}</span> {/* 계좌번호 표시 */}
+            </span>
+        ) : (
+            <span>{item.account}</span> /* 매칭되지 않은 경우 계좌번호만 반환 */
+        );
+    
+        return {
+            ...item,
+            account: updatedAccount, // 계좌번호에 SVG 포함
+        };
+    });
+    
+
     const tableHeaders = {
         name: '직원 이름',
         account: '계좌번호',
@@ -80,9 +102,9 @@ export default function PayRecords() {
             </div>
 
             {/* 테이블 영역 */}
-            <DefaultTable 
-                tableHeaders={tableHeaders} 
-                list={sortedList} 
+            <DefaultTable
+                tableHeaders={tableHeaders}
+                list={updatedList}
             />
         </div>
     );
