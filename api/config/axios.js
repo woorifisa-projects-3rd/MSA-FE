131f88aaccc5e765ae9819e91f9122af9019ab00
hyeri1126 <114209093+hyeri1126@users.noreply.github.com>
2024-11-18 12:31:49 +0900

    feat:이메일(아이디) 찾기 post 요청 성공

--- api/config/axios.js
+++ api/config/axios.js
@@ -16,12 +16,18 @@ const apiClient  = axios.create({
     }
 });
 
-// 요청 인터셉터 - 요청 보내기 전 수행 -> JWT 토큰 관리
-apiClient.interceptors.response.use(
+// 요청 인터셉터 - 요청 보내기 전 수행 
+// 요청 전 JWT 토큰을 가지고 있는지 매번 확인!
+apiClient.interceptors.request.use(
     (config) => {
-        if(config.url === '/api/auth/login'){
+        // ID 찾기, 로그인 등 인증이 필요 없는 경로들
+        const publicPaths = ['/user/president/id-find', '/user/president/login'];
+        // 인증이 필요없는 경로라면 토큰 검사 없이 진행
+        if(publicPaths.includes(config.url)){
             return config
         }
+       
+        // 그 외 인증이 필요한 경로에 대해서는 토큰 체크! 
 
         // localstorage에서 토큰 가져오기
         const token = localStorage.getItem('token');
@@ -29,10 +35,7 @@ apiClient.interceptors.response.use(
         // 토큰이 있다면 Authorization 헤더에 추가
         if(token){
             config.headers.Authorization = `Bearer ${token}`;
-        } else {
-             // 토큰이 없으면 로그인 페이지로 리다이렉트
-             window.location.href = '/login';
-        }
+        } 
 
         return config;
     },
@@ -42,50 +45,50 @@ apiClient.interceptors.response.use(
 )
 
 // 응답 인터셉터 - spring boot 서버의 응답 처리
-apiClient.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    // 응답에 대한 에러 핸들링 
-    (error) => {
-        if (error.response) {
-            switch (error.response.status) {
-                case 400: // Bad Request
-                    console.error('잘못된 요청입니다.');
-                    break;
+// apiClient.interceptors.response.use(
+//     (response) => {
+//         return response;
+//     },
+//     // 응답에 대한 에러 핸들링 
+//     (error) => {
+//         if (error.response) {
+//             switch (error.response.status) {
+//                 case 400: // Bad Request
+//                     console.error('잘못된 요청입니다.');
+//                     break;
                     
-                case 401: // 토큰이 유효하지 않거나 만료된 경우
-                    localStorage.removeItem('token'); // 토큰 삭제
-                    window.location.href = '/login';  // 로그인 페이지로 리다이렉트
-                    break;
+//                 case 401: // 토큰이 유효하지 않거나 만료된 경우
+//                     localStorage.removeItem('token'); // 토큰 삭제
+//                     window.location.href = '/login';  // 로그인 페이지로 리다이렉트
+//                     break;
                     
-                case 403: // Forbidden
-                    console.error('권한이 없습니다.');
-                    break;
+//                 case 403: // Forbidden
+//                     console.error('권한이 없습니다.');
+//                     break;
                     
-                case 404: // Not Found
-                    console.error('요청한 리소스를 찾을 수 없습니다.');
-                    break;
+//                 case 404: // Not Found
+//                     console.error('요청한 리소스를 찾을 수 없습니다.');
+//                     break;
                     
-                case 500: // Internal Server Error
-                    console.error('서버 에러가 발생했습니다.');
-                    break;
+//                 case 500: // Internal Server Error
+//                     console.error('서버 에러가 발생했습니다.');
+//                     break;
                     
-                default:
-                    console.error('알 수 없는 에러가 발생했습니다.');
-                    break;
-            }
-        } else if (error.request) {
-            // 요청은 보냈으나 응답을 받지 못한 경우
-            console.error('서버로부터 응답이 없습니다.');
-        } else {
-            // 요청 설정 중 에러 발생
-            console.error('요청 설정 중 에러가 발생했습니다:', error.message);
-        }
+//                 default:
+//                     console.error('알 수 없는 에러가 발생했습니다.');
+//                     break;
+//             }
+//         } else if (error.request) {
+//             // 요청은 보냈으나 응답을 받지 못한 경우
+//             console.error('서버로부터 응답이 없습니다.');
+//         } else {
+//             // 요청 설정 중 에러 발생
+//             console.error('요청 설정 중 에러가 발생했습니다:', error.message);
+//         }
         
-        return Promise.reject(error);  // promise를 실패 상태로 바꾸는 함수
-    }
-)
+//         return Promise.reject(error);  // promise를 실패 상태로 바꾸는 함수
+//     }
+// )
 
 // 추후 추가 예정 
 // 특정 요청을 위한 axios 인스턴스 
