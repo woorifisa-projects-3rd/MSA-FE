fbfe43879d8ed1ca415264c88c2f9289dc66d6bf
hyeri1126 <hyeri1126@ewhain.net>
2024-11-20 09:33:27 +0900

    fix: apiRouteClient 추가

--- app/api/mypage/route.js
+++ app/api/mypage/route.js
@@ -3,14 +3,9 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request) {
     try {
-        // 서버 사이드에서 안전하게 토큰 접근
-        const token = cookies().get('accessToken')?.value;
 
-        console.log('API Route에서 토큰:', token);
-        const headers = new Headers(request.headers);
-        
         // Spring Boot 서버로 요청
-        const response = await fetch('http://localhost:8888/user/president/mypage', {
+        const response = await ('http://localhost:8888/user/president/mypage', {
             headers
         });
 
