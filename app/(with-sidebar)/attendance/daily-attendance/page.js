6f6b99e3977c627507e313d0c3b82bf11c00ef88
hyeri1126 <114209093+hyeri1126@users.noreply.github.com>
2024-11-26 16:27:42 +0900

    fix:daily-attendance page.js getServerSideProps 함수 추가

--- app/(with-sidebar)/attendance/daily-attendance/page.js
+++ app/(with-sidebar)/attendance/daily-attendance/page.js
@@ -1,22 +1,48 @@
 'use client';
 
-import PrimaryButton from '@/components/button/primary-button';
 import AttendanceModalBody from '@/components/modal/attendance-modal/attendance-modal-body';
 import ModalContainer from '@/components/modal/modal-container';
 import DefaultTable from '@/components/table/DefaultTable';
-import { useState, useEffect , useCallback} from 'react';
-import { useSearchParams } from 'next/navigation';
+import { useState} from 'react';
 import { nextClient } from '@/lib/nextClient';
 
-export default function Form() {
+// 렌더링 전 서버에서 데이터 가져오기
+export async function getServerSideProps(context) {
+  const selectedDate = context.query.date || null;
+  let formattedList = [];
+
+  if(selectedDate){
+    try{
+      const response = await nextClient.get(
+        `/attendance/daily-attendance?storeid=1&commutedate=${selectedDate}`
+      );
+      console.log("client가 next-servr로부터 받은 data",response)
+      
+      formattedList = response.data.map((item, index) => ({
+        no: (index + 1).toString().padStart(2, '0'),
+        name: item.name,
+        startTime: item.startTime.substring(11, 16),
+        endTime: item.endTime.substring(11, 16),
+        totalHours: item.totalHours,
+        salary: `${item.salary}원`,
+      }));
+    } catch(error){
+      console.error('서버에서 데이터를 가져오는 중 오류 발생:', error);
+    }
+  }
+      
+  return { 
+    props: { selectedDate, attendanceList:formattedList},
+  };
+
+}
+
+export default function Form({selectedDate, attendanceList}) {
   const [isModalOpen, setIsModalOpen] = useState(false); 
   const [isEditModalOpen, setEditModalOpen] = useState(false);
   const [selectedAttendance, setSelectedAttendance] = useState(null);
-  const searchParams = useSearchParams();
-  const selectedDate = searchParams.get('date'); // 쿼리 파라미터에서 날짜 추출
+  const [formData, setFormData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
-
-  
 
   const tableHeaders = {
     no: "No.",
@@ -29,104 +55,14 @@ export default function Form() {
     delete: "삭제"
 };
 
-// api 요청시 받아올 데이터 - 현재는 더미데이터
-  // const list = [
-  //   {
-  //       no: "01",
-  //       name: "정성윤",
-  //       startTime: "14:00",
-  //       endTime: "18:04",
-  //       totalHours: "04:04",
-  //       salary: "38000원",
-  //   },
-  //   {
-  //       no: "02",
-  //       name: "류혜리",
-  //       startTime: "08:58",
-  //       endTime: "15:03",
-  //       totalHours: "06:05",
-  //       salary: "60000원",
-  //   },
-  //   {
-  //       no: "02",
-  //       name: "류혜리",
-  //       startTime: "08:58",
-  //       endTime: "15:03",
-  //       totalHours: "06:05",
-  //       salary: "60000원",
-  //   },
-  //   {
-  //       no: "02",
-  //       name: "류혜리",
-  //       startTime: "08:58",
-  //       endTime: "15:03",
-  //       totalHours: "06:05",
-  //       salary: "60000원",
-  //   },
-  //   {
-  //       no: "02",
-  //       name: "류혜리",
-  //       startTime: "08:58",
-  //       endTime: "15:03",
-  //       totalHours: "06:05",
-  //       salary: "60000원",
-  //   }
-  // ];
 
-  // const changedList = list.map((list)=>({
-  //  ...list,
-  //  edit:(
-  //   <PrimaryButton
-  //     text="편집"
-  //     onClick={() => {
-  //       setSelectedAttendance(list)
-  //       setEditModalOpen(true)
-  //     }}
-  //   />
-  //  )
-  // }))
-  // 일별로 출퇴근한 데이터 조회 관련 state 및 함수
-  const [attendanceList, setAttendanceList] =  useState([]);
-
-  const fetchDailyAttendance = useCallback(async () => {
-    try {
-      const response = await nextClient.get(
-        `/attendance/daily-attendance?storeid=1&commutedate=${selectedDate}`
-      );
-  
-      console.log("client가 next-servr로부터 받은 data",response)
-      
-      const formattedList = response.data.map((item, index) => ({
-        no: (index + 1).toString().padStart(2, '0'),
-        name: item.name,
-        startTime: item.startTime.substring(11, 16),
-        endTime: item.endTime.substring(11, 16),
-        totalHours: item.totalHours,
-        salary: `${item.salary}원`,
-      }));
-  
-      setAttendanceList(formattedList);
-    } catch (error) {
-      console.error('Error fetching daily attendance:', error);
-    }
-  }, [selectedDate]); // selectedDate를 dependency로 추가
-  
-  // 그리고 useEffect의 dependency array에 fetchDailyAttendance 추가
-  useEffect(() => {
-    if (selectedDate) {
-      fetchDailyAttendance();
-    }
-  }, [selectedDate, fetchDailyAttendance]);
-
-
-  // 출퇴근 기록 추가 POST 요청 관련 state 및 함수 
-  const [formData, setFormData] = useState({});
-  
+  // 여기부터 출퇴근 기록 추가 post 요청 관련 함수
   const handleFormChange = (updatedData) => {
     setFormData(updatedData);
   };
 
 
+  // post 요청은 ssr 이후에 처리 (client측에서)
   const handleSubmit = async () => {
     setIsLoading(true);
 
@@ -157,8 +93,6 @@ export default function Form() {
     }
   };
 
-
- 
 
   return (
     <>
