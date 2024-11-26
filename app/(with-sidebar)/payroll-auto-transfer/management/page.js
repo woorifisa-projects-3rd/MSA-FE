18d3d4a6026b4f4575663e6fb07df81b7f84886e
hyeri1126 <114209093+hyeri1126@users.noreply.github.com>
2024-11-26 17:37:36 +0900

    add:급여 자동이체 관리 페이지 추가

---
+++ app/(with-sidebar)/payroll-auto-transfer/management/page.js
@@ -0,0 +1,14 @@
+export default function Home() {
+    return (
+     <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
+     
+        <div style={{ backgroundColor: '#f0f8ff', padding: '15px', borderRadius: '8px', color: '#333', marginBottom: '10px' }}>
+          ddddddddddddddddddd
+        </div>
+        
+        <div style={{ height:'100vh', backgroundColor: '#ffebcd', padding: '15px', borderRadius: '8px', color: '#333' }}>
+            여긴 mypage
+        </div>
+     </div>
+    );
+}
