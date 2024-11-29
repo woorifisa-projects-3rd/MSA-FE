7a3dcf47508e3438d5296b3920c344a2f1ff7c3e
hyeri1126 <114209093+hyeri1126@users.noreply.github.com>
2024-11-29 13:02:38 +0900

    fix:addressSearch 수정

--- components/modal/workplace-registration.js/steps/AddressStep.js
+++ components/modal/workplace-registration.js/steps/AddressStep.js
@@ -1,24 +1,52 @@
 import { useRegistration } from "@/contexts/RegistrationContext"
 import styles from "../BusinessRegistration.module.css"
 import AddressSearch from "@/components/addsearch/AddressSearch";
+import { getGeocode } from "@/utils/getGeocode";
+import { useState } from "react";
 
 export default function AddressStep(){
     const { formData, setFormData } = useRegistration();
+    const [selectedAddress, setSelectedAddress] = useState('');
 
-   // 주소 변경 핸들러
-   const handleAddressChange = (postcodeAddress, detailAddress) => {
-    setFormData(prev => ({
-        ...prev,
-        location: `${postcodeAddress} ${detailAddress}`.trim()
-        // 필요한 경우 latitude, longitude도 여기서 설정
-    }));
-};
+    const handleAddressComplete = async (postcodeAddress) => {
+        try{
+            const coordinates = await getGeocode(postcodeAddress);
+            console.log(coordinates);
+
+            setSelectedAddress(postcodeAddress);
+            setFormData(prev => ({
+                ...prev,
+                location: postcodeAddress,
+                latitude: coordinates.lat,
+                longitude: coordinates.lng
+            }))
+        }catch (error) {
+            console.error("주소 좌표 변환 실패:", error);
+            setFormData(prev => ({
+                ...prev,
+                latitude: 0,
+                longitude: 0
+            }));
+        }
+    }
+
+    // 상세 주소 변경 핸들러
+    const handleDetailChange  = (updatedDetail) => {
+        if (!selectedAddress) return;
+
+        setFormData(prev => ({
+            ...prev,
+            location: updatedDetail  ? `${selectedAddress}, ${updatedDetail}` : selectedAddress
+        }))
+    }
+
 
     return(
         <div className={styles.formContainer}>
             <h2 className={styles.formTitle}>주소 입력</h2>
             <AddressSearch 
-                onAddressChange={handleAddressChange}
+                onDetailChange={handleDetailChange}
+                onAddressComplete={handleAddressComplete}
                 initialPostcodeAddress=""
                 initialDetailAddress=""
             />
