0757d4d2e2475b4e885de0595a17372364c1862b
Hyunah Lee <hyuna030619@naver.com>
2024-12-03 15:05:03 +0900

    feat: 회원가입 페이지 이메일 인증 서버 응답 수정

--- app/(no-sidebar)/(auth)/signup/page.js
+++ app/(no-sidebar)/(auth)/signup/page.js
@@ -95,32 +95,18 @@ export default function Signup() {
       const response = await nextClient.post('/auth/signup/email', {
         email: formData.email,
       });
-
-      console.log(response.data);
   
       // 성공 응답 처리
-      if (response.data.success) {
-        if (response.data.pin.code === 'EMAIL_ALREADY_EXISTS') {
-          setFormErrors((prevErrors) => ({
-            ...prevErrors,
-            email: '이미 등록된 이메일입니다. 다른 이메일을 사용해주세요.',
-          }));
-
-        } else {
-          // alert('이메일을 발송했습니다!');
-          setEmailSuccess('이메일이 발송되었습니다. 확인해주세요.');
-          setEmailConfirmNumber(response.data.pin); // PIN 설정
-          setError(''); // 에러 상태 초기화
-        }
-      } else {
-        // 백엔드에서 `success: false` 반환 시
-        throw new Error(response.data.pin.error || '이메일 전송 실패');
-      }
+      setEmailSuccess('이메일이 발송되었습니다. 확인해주세요.');
+      setEmailConfirmNumber(response.data.pin); // PIN 설정
+      setError(''); // 에러 상태 초기화
     } catch (error) {
       // 에러 처리
       const errorMessage = error.response?.data?.error || error.message;
-      setError(errorMessage); // 에러 메시지 상태 업데이트
-      alert(errorMessage); // 사용자에게 에러 메시지 표시
+      setFormErrors((prevErrors) => ({
+        ...prevErrors,
+        email: errorMessage,
+      }));
     }
   };
   
