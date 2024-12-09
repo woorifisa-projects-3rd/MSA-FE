7bd9345d3a89fdc7d5c9b2aa6a99454b409a6b5c
hyeri1126 <114209093+hyeri1126@users.noreply.github.com>
2024-12-09 15:31:03 +0900

    직원 추가 에러 해결, 로그인 시 매출/지출 페이지로 redirect

--- app/(no-sidebar)/(auth)/login/page.js
+++ app/(no-sidebar)/(auth)/login/page.js
@@ -31,7 +31,7 @@ export default function LoginPage() {
             });
         
             if (response.data.success) {
-                router.push('/mypage');
+                router.push('/transactions/analytics');
                 if (email === 'zipgyesajang@gmail.com') {
                     router.push('/manager');
                 }
